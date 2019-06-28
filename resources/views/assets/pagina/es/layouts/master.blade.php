63094d6a1765f454c467e0c161c0bacc859d646a
hamely <sarmiento101147@gmail.com>
2019-06-28 11:33:37 -0500

    stilo color

--- resources/views/assets/pagina/es/layouts/master.blade.php
+++ resources/views/assets/pagina/es/layouts/master.blade.php
@@ -104,7 +104,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 <div class="footer">
   <div class="footer_agile_inner_info_w3l">
-    <div class="col-md-3 footer-left">
+    <!-- <div class="col-md-3 footer-left">
       <h2><a href="index.html"><span>E</span>lite Shoppy </a></h2>
       <p>Lorem ipsum quia dolor
       sit amet, consectetur, adipisci velit, sed quia non 
@@ -123,8 +123,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                                   <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
                                   <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
                             </ul>
-    </div>
-    <div class="col-md-9 footer-right">
+    </div> -->
+    <!-- <div class="col-md-9 footer-right">
       <div class="sign-grds">
         <div class="col-md-4 sign-gd">
           <h4>Our <span>Information</span> </h4>
@@ -191,22 +191,39 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         </div>
         <div class="clearfix"></div>
       </div>
-    </div>
-    <div class="clearfix"></div>
+    </div> -->
+  <!-- <div class="clearfix"></div> -->
       <div class="agile_newsletter_footer">
           <div class="col-sm-6 newsleft">
-        <h3>SIGN UP FOR NEWSLETTER !</h3>
+        <!-- <h3>SIGN UP FOR NEWSLETTER !</h3> -->
+        <div class="col-md-6 footer-left">
+          
+          <ul class="social-nav model-3d-0 footer-social w3_agile_social two">
+                                  <li><a href="#" class="facebook">
+                                      <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+                                      <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+                                  <li><a href="#" class="twitter"> 
+                                      <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+                                      <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+                                  <li><a href="#" class="instagram">
+                                      <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+                                      <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
+                                  <li><a href="#" class="pinterest">
+                                      <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+                                      <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
+                                </ul>
+        </div>
       </div>
       <div class="col-sm-6 newsright">
         <form action="#" method="post">
-          <input type="email" placeholder="Enter your email..." name="email" required="">
+          <input type="email" placeholder="Ingrese un correo" name="email" required="">
           <input type="submit" value="Submit">
         </form>
       </div>
 
     <div class="clearfix"></div>
   </div>
-    <p class="copy-right">&copy 2017 Elite shoppy. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+    <p class="copy-right">2019 Todos los derechos reservados | <a href=" "></a></p>
   </div>
 </div>
 <!-- //footer -->
